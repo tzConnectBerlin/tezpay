42d8983d99255699ffc40af0088a487857531309
Rick Klomp <rick.klomp@tzconnect.com>
2022-05-12 13:25:09 +0200

    bugfix amend: cast to BigInt because Number may not be sufficient
    maximum number that Number can represent is (2^53 - 1) [0], or approx. 9e15.
    mutez amount currently in circulation is approx 8e14. So we should be
    fine with Number but I'd say it's not worth it to be so close to the
    theoratical limits.
    
    [0]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER

--- server/data_access.mjs
+++ server/data_access.mjs
@@ -21,14 +21,14 @@ export default function({db, paypoint_schema_name}) {
 
 	const get_payment = async function({ external_id }) {
 		let result = (await db.query(GET_PAYMENT_SQL, [external_id])).rows[0];
-		result.mutez_amount = Number(result.mutez_amount);
+		result.mutez_amount = BigInt(result.mutez_amount);
 		return result;
 	};
 
 	const get_fulfillments = async function ({ message, block_confirmations }) {
 		let result = await db.query(GET_FULFILLMENTS_SQL, [message, block_confirmations])
 		return result.rows.map(row => {
-			row.amount = Number(row.amount);
+			row.amount = BigInt(row.amount);
 			return row;
 		});
 	}
